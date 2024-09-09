7eca2eae842a82cae64e7c9bf317d48ec29648e5
Eden Lumbroso <edenlum@gmail.com>
2024-09-09 12:14:11 +0300

    Base code commit

---
+++ datasets.py
@@ -0,0 +1,236 @@
+import torch
+from torch.utils.data import Dataset
+import unittest
+import numpy as np
+
+
+def delay_l2(lag):
+    def delay_loss_func(output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        loss = torch.mean((output[:, lag:, :] - target[:, lag:, :]) ** 2)
+        return loss
+
+    return delay_loss_func
+
+
+class NormalNoiseSignalGenerator():
+    def __init__(self, std=1, mean=0):
+        self.std = std
+        self.mean = mean
+
+    def generate(self, num_signals, signal_length, signal_dim=1):
+        if signal_dim != 1:
+            raise NotImplementedError("ConstSignalGenerator only supports signal dim 1")
+
+        signals = torch.randn([num_signals, signal_length])
+        return signals
+
+
+class DelayedSignalDatasetRegenerated(torch.utils.data.TensorDataset):
+
+    def __init__(self, samples_num=1, seq_length=10000, lag_length=1000,
+                 signal_generator=None, lag_type="zero"):
+        assert lag_length < seq_length
+        assert lag_type in ["zero"]
+
+        if signal_generator is None:
+            signal_generator = NormalNoiseSignalGenerator()
+            # raise ValueError("signal_generator must be provided")
+
+        self.signal_generator = signal_generator
+        self.samples_num = samples_num
+        self.seq_length = seq_length
+        self.lag_length = lag_length
+        self.lag_type = lag_type
+        super().__init__()
+
+    def __getitem__(self, index):
+        X = self.signal_generator.generate(num_signals=1,
+                                           signal_length=self.seq_length)
+        X = X.unsqueeze(-1)
+
+        if self.lag_type == "zero":
+            Y = torch.zeros(X.shape)
+            Y[:, self.lag_length:, :] = X[:, :-self.lag_length, :]
+
+        return X[0, :, :], Y[0, :, :]
+
+    def __len__(self):
+        return self.samples_num
+
+
+def logits_to_probability(logits):
+    # Apply softmax to convert logits to probabilities
+    probabilities = F.softmax(logits, dim=-1)
+    # Convert the PyTorch tensor to a NumPy array
+    probabilities_numpy = probabilities.detach().numpy()
+    return probabilities_numpy
+
+
+class DynamicCategoricalDataset(Dataset):
+    def __init__(self, epoch_size, seq_len, n_categories, lag, auto_regressive=False, copy_token=False):
+        """
+        Initialize the dataset with the parameters for data generation.
+        Args:
+            epoch_size (int): The total number of examples (data points).
+            seq_len (int): The length of each sequence.
+            n_categories (int): The number of categories for each element in the sequence.
+            lag (int): The number of steps to shift the data for label generation.
+        """
+        self.amount_of_examples = epoch_size
+        self.seq_len = seq_len
+        self.cat_num = n_categories
+        self.lag = lag
+        self.auto_regressive = auto_regressive
+        self.copy_token = copy_token
+
+    def __len__(self):
+        """
+        Return the total number of examples you want the loader to simulate.
+        """
+        return self.amount_of_examples
+
+    def __getitem__(self, idx):
+        """
+        Generates a single data point on demand.
+        """
+        data = np.random.randint(1, self.cat_num, size=(self.seq_len,))
+
+        if self.copy_token:
+            data[0] = 0
+            data[self.lag] = 0
+        if not self.auto_regressive:
+            labels = np.zeros_like(data)
+            if self.lag < self.seq_len:
+                labels[self.lag:] = data[:-self.lag]
+        else:
+            data[self.lag:] = data[:-self.lag]
+            data, labels = data[:-1], data[1:]
+        return torch.tensor(data, dtype=torch.long), torch.tensor(labels, dtype=torch.long)
+
+
+class InductionHead(Dataset):
+    def __init__(self, epoch_size, seq_len, n_categories, num_triggers, induction_length, auto_regressive=True):
+        """
+        Initialize the Induction Head dataset with the parameters for data generation.
+        Args:
+            epoch_size (int): The total number of examples (data points).
+            seq_len (int): The length of each sequence.
+            n_categories (int): The number of categories for each element in the sequence.
+            num_triggers (int): The (maximum) number of times the copy token is shown in the sequence.
+            induction_length (int): The length of the induction sequence (after the copy token).
+        """
+        self.amount_of_examples = epoch_size
+        self.seq_len = seq_len
+        self.cat_num = n_categories
+        self.num_triggers = num_triggers
+        self.induction_length = induction_length
+        self.auto_regressive = auto_regressive
+        self.copy_token = 0
+
+    def __len__(self):
+        """
+        Return the total number of examples you want the loader to simulate.
+        """
+        return self.amount_of_examples
+
+    def __getitem__(self, idx):
+        """
+        Generates a single data point on demand.
+        """
+        data = np.random.randint(1, self.cat_num, size=(self.seq_len,))
+        data = np.append(data, 0)
+        copy_indices = np.random.choice(np.arange(0, self.seq_len - self.induction_length), size=self.num_triggers,
+                                        replace=False)
+        copy_indices_filtered = []
+        for i, p in enumerate(copy_indices):
+            if i == 0:
+                copy_indices_filtered.append(p)
+            elif p - copy_indices_filtered[-1] > self.induction_length:
+                copy_indices_filtered.append(p)
+        to_copy = [
+            data[copy_indices_filtered[0] + 1 + i]
+            for i in range(self.induction_length)
+        ]
+        for pos in copy_indices_filtered:
+            data[pos] = self.copy_token
+            for i in range(self.induction_length):
+                data[pos + 1 + i] = to_copy[i]
+        if self.auto_regressive:
+            data = np.concatenate((data, to_copy))
+            data, labels = data[:-1], data[1:]
+        else:
+            labels = np.concatenate((data, to_copy))
+            data = np.concatenate((data, np.random.randint(1, self.cat_num, size=(self.induction_length,))))
+        return torch.tensor(data, dtype=torch.long), torch.tensor(labels, dtype=torch.long)
+
+
+AR = [True, False]
+
+
+class TestInductionHead(unittest.TestCase):
+
+    def setUp(self):
+        # Initialize the dataset with specific parameters
+        self.epoch_size = 100
+        self.seq_len = 10
+        self.n_categories = 5
+        self.num_triggers = 1
+        self.induction_length = 9
+        self.dataset = InductionHead(self.epoch_size, self.seq_len, self.n_categories, self.num_triggers,
+                                     self.induction_length, auto_regressive=False)
+
+    def test_initialization(self):
+        # Check initialization parameters
+        self.assertEqual(self.dataset.amount_of_examples, self.epoch_size)
+        self.assertEqual(self.dataset.seq_len, self.seq_len)
+        self.assertEqual(self.dataset.cat_num, self.n_categories)
+        self.assertEqual(self.dataset.num_triggers, self.num_triggers)
+        self.assertEqual(self.dataset.induction_length, self.induction_length)
+
+    def test_dataset_length(self):
+        # Test the dataset length
+        self.assertEqual(len(self.dataset), self.epoch_size)
+
+    def test_getitem(self):
+        # Test properties of generated data points
+        data, labels = self.dataset.__getitem__(0)
+        self.assertEqual(data.shape[0],
+                         self.seq_len + 1 + self.induction_length + (self.dataset.auto_regressive * (- 1)))
+        self.assertEqual(labels.shape[0],
+                         self.seq_len + 1 + self.induction_length + (self.dataset.auto_regressive * (- 1)))
+        self.assertTrue((data >= 0).all() and (data < self.n_categories).all())
+        self.assertTrue((labels >= 0).all() and (labels < self.n_categories).all())
+
+        # Check for presence of copy token
+        copy_token_positions = (data == self.dataset.copy_token).nonzero()[0]
+        self.assertTrue(len(copy_token_positions) <= self.num_triggers)
+        for pos in copy_token_positions:
+            one = 1 if self.dataset.auto_regressive else 0
+            print(data[pos + one:pos + one + self.induction_length], labels[pos:pos + self.induction_length])
+            self.assertTrue(np.array_equal(data[pos + one:pos + one + self.induction_length],
+                                           labels[pos:pos + self.induction_length]))
+
+
+class TestDelay(unittest.TestCase):
+
+    def setUp(self):
+        # Initialize the dataset with specific parameters
+        self.epoch_size = 100
+        self.seq_len = 10
+        self.n_categories = 5
+        self.num_triggers = 1
+        self.lag = 5
+        self.dataset = DynamicCategoricalDataset(self.epoch_size, self.seq_len, self.n_categories, self.lag,
+                                                 auto_regressive=True, copy_token=True)
+
+    def test_getitem(self):
+        # Test properties of generated data points
+        data, labels = self.dataset.__getitem__(0)
+        self.assertTrue((data >= 0).all() and (data < self.n_categories).all())
+        self.assertTrue((labels >= 0).all() and (labels < self.n_categories).all())
+
+        self.assertTrue(np.array_equal(data[:-self.lag + 1], labels[self.lag:]))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ utils.py
@@ -0,0 +1,176 @@
+from dataclasses import dataclass
+import itertools
+import ray
+from ray.actor import ActorHandle
+from asyncio import Event
+from typing import Tuple
+from tqdm import tqdm
+from copy import deepcopy
+import hashlib
+import json
+
+
+def get_hash(config):
+    # Convert the dictionary to a JSON string
+    json_data = json.dumps(config, sort_keys=True)  # sort_keys ensures consistent order
+
+    # Generate the hash (using SHA256 as an example)
+    hash_object = hashlib.sha256(json_data.encode('utf-8'))
+    hash_value = hash_object.hexdigest()
+    return hash_value
+
+
+@dataclass
+class Config:
+    ssm_type: str
+    initA_real: str
+    initA_imag: str
+    discretizationA: str
+    discretizationB: str
+    param_A_imag: str
+    A_imag_using_weight_decay: str
+    dt_is_selective: str
+    channel_sharing: str
+    deterministic: bool
+    pscan: bool
+    d_model: int
+    d_state: int
+    n_layers: int
+    n_categories: int
+    lag: int
+    extra: int
+    batch_size: int
+    epoch_size: int
+    epochs: int
+    lr: float
+    stop_on_loss: float
+    seed: int
+    comment: str
+    bias: bool
+
+
+def experiments(kwargs):
+    # Extract argument names and their corresponding value lists
+    arg_names = [k[0] for k in kwargs]
+    value_lists = [k[1] for k in kwargs]
+
+    # Iterate over the Cartesian product of value lists
+    for values in itertools.product(*value_lists):
+        # Yield a dictionary mapping argument names to values
+        yield dict(zip(arg_names, values))
+
+
+def override_config(config, args):
+    # return a new config with the values in args updated
+    config = deepcopy(config)
+    for arg in args:
+        key, value = arg.split('=', 1)
+        value = convert_type(value)
+        keys = key.split('.')
+        sub_config = config
+        for k in keys[:-1]:
+            sub_config = sub_config.setdefault(k, {})
+        sub_config[keys[-1]] = value
+    return config
+
+
+def convert_type(value):
+    # Convert to integer
+    try:
+        return int(value)
+    except ValueError:
+        pass
+
+    # Convert to boolean
+    if value.lower() in ['true', 'false']:
+        return value.lower() == 'true'
+
+    # Return string if no conversion possible
+    return value
+
+
+@ray.remote
+class ProgressBarActor:
+    counter: int
+    delta: int
+    event: Event
+
+    def __init__(self) -> None:
+        self.counter = 0
+        self.delta = 0
+        self.event = Event()
+
+    def update(self, num_items_completed: int = 1) -> None:
+        """Updates the ProgressBar with the incremental
+        number of items that were just completed.
+        """
+        self.counter += num_items_completed
+        self.delta += num_items_completed
+        self.event.set()
+
+    async def wait_for_update(self) -> Tuple[int, int]:
+        """Blocking call.
+
+        Waits until somebody calls `update`, then returns a tuple of
+        the number of updates since the last call to
+        `wait_for_update`, and the total number of completed items.
+        """
+        await self.event.wait()
+        self.event.clear()
+        saved_delta = self.delta
+        self.delta = 0
+        return saved_delta, self.counter
+
+    def get_counter(self) -> int:
+        """
+        Returns the total number of complete items.
+        """
+        return self.counter
+
+
+class ProgressBar:
+    """
+    Usage example:
+    >> pb = ProgressBar()
+    >> futures = [somefunc.remote(someinput, pb.actor) for someinput in someinputs]
+    >> pb.set_total(len(futures))
+    >> pb.print_until_done()
+    """
+    progress_actor: ActorHandle
+    total: int
+    description: str
+    pbar: tqdm
+
+    def __init__(self, description: str = ""):
+        # Ray actors don't seem to play nice with mypy, generating
+        # a spurious warning for the following line,
+        # which we need to suppress. The code is fine.
+        self.progress_actor = ProgressBarActor.remote()  # type: ignore
+        self.description = description
+
+    def set_total(self, total: int):
+        self.total = total
+
+    @property
+    def actor(self) -> ActorHandle:
+        """Returns a reference to the remote `ProgressBarActor`.
+
+        When you complete tasks, call `update` on the actor.
+        """
+        return self.progress_actor
+
+    def print_until_done(self) -> None:
+        """Blocking call.
+
+        Do this after starting a series of remote Ray tasks, to which you've
+        passed the actor handle. Each of them calls `update` on the actor.
+        When the progress meter reaches 100%, this method returns.
+        """
+        pbar = tqdm(desc=self.description, total=self.total,
+                    smoothing=0)
+        while True:
+            delta, counter = ray.get(self.actor.wait_for_update.remote())
+            pbar.update(delta)
+            if counter >= self.total:
+                pbar.close()
+                return
